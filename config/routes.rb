a7b0c57652a75d709a0813052d4ee60ff59081e2
Ygor Geurts <ygor.geurts@gmail.com>
2012-06-11 15:44:08 +0200

    minor change

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ class Spree::PossiblePage
   def matches?(request)
     non_page_routes = ['admin', 'account', 'cart', 'checkout', 'content', 'login', 'pg/', 'orders', 'products', 's/', 'session', 'signup', 'shipments', 'states', 't/', 'tax_categories', 'user']
     non_page_routes.each{|r| return false if request.path.include?(r)}
-    !Spree::Page.active.find_by_path(request.path).nil?
+    Spree::Page.active.find_by_path(request.path).present?
   end
 end
 
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-
     resources :pages, :constraints => { :id => /.*/ } do
       collection do
         post :update_positions
@@ -29,5 +28,5 @@ Spree::Core::Engine.routes.draw do
   end
 
   resources :pages
-  match '*page_path', :to => "pages#show", :as => :page, :constraints => Spree::PossiblePage.new
+  get '*page_path', :to => "pages#show", :as => :page, :constraints => Spree::PossiblePage.new
 end
